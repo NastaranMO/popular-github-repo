4eb2e600db92d1fb8340e2c57bbc5bcaabd756c2
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2021-08-31 13:24:10 +0200

    add selectedLanguage nav bar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,14 +2,40 @@ import React from "react";
 import "../index.css";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+
+    this.handler = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage });
+  }
+
   render() {
     const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
-
+    console.log(this.state);
     return (
       <ul className="flex-center">
         {languages.map((language) => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button
+              style={
+                language === this.state.selectedLanguage
+                  ? {
+                      color: "rgb(187, 46, 31)",
+                    }
+                  : null
+              }
+              className="btn-clear nav-link"
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
